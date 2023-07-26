9022e617ad2cd35146ad6181e7cbf0d1dce01a91
Titouan-Loeb <titouan.loeb@epitech.eu>
2023-07-27 07:12:52 +0900

    [ADD] base running for the bot

---
+++ README.md
@@ -0,0 +1,21 @@
+# Discord Role Play Bot
+
+## About
+
+This is just a simple bot to use for role play games
+
+## Install
+
+To install and test the bot you need to have nodejs and npm installed on your computer
+
+```bash
+npm install
+```
+
+## Run
+
+To run the bot run the following command
+
+```bash
+npm start
+```
---
+++ src/bot.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+const fs = require("fs");
+const { Client, Collection, GatewayIntentBits } = require("discord.js");
+const token = process.env.DISCORD_TOKEN;
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers,
+  ],
+});
+client.commands = new Collection();
+client.commandArray = [];
+
+const functionFolders = fs.readdirSync("./src/functions");
+for (const folder of functionFolders) {
+  const functionFiles = fs
+    .readdirSync(`./src/functions/${folder}`)
+    .filter((file) => file.endsWith(".js"));
+  for (const file of functionFiles)
+    require(`./functions/${folder}/${file}`)(client);
+}
+
+client.handleCommands();
+client.handleEvents();
+client.login(token);
